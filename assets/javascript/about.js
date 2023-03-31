316ad9e7eb2ca35f71db13b285bb302cbc581fad
michaelcgre <124597287+michaelcgre@users.noreply.github.com>
2023-03-30 20:57:12 -0400

    light-dark-mode-and-contact-form

--- assets/javascript/about.js
+++ assets/javascript/about.js
@@ -5,3 +5,14 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
+
+const icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.className = "fa-solid fa-sun";
+    } else {
+        icon.className = "fa-solid fa-moon";
+    }
+};
--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -5,3 +5,20 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
+
+const icon = document.getElementById("icon");
+const portrait = document.getElementById("portrait");
+const desktopPortrait = document.getElementById("desktop-portrait");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.className = "fa-solid fa-sun";
+        portrait.src = "assets/images/white-portrait.png";
+        desktopPortrait.srcset = "assets/images/white-portrait-lg.png";
+    } else {
+        icon.className = "fa-solid fa-moon";
+        portrait.src = "assets/images/portrait-with-border-md-sm.png";
+        desktopPortrait.srcset = "assets/images/portrait-with-border-lg.png";
+    }
+};
